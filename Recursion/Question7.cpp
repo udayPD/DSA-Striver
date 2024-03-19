7ad7904d9eb3459edca1bbb653df773cd0cf4c69
Uday Sankor Mohan <98579315+udayPD@users.noreply.github.com>
2024-03-19 17:05:18 +0530

    Add files via upload

---
+++ Recursion/Question7.cpp
@@ -0,0 +1,16 @@
+//Print the factorial of n numbers using recursion
+
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    if( n== 0){
+      return 1;
+    }
+    return n + fact(n-1);
+}
+int main(){
+    int n;
+    cin >> n;
+    cout << fact(n);
+}
